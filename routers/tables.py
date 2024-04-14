6c2eb13e51916f521ef7f1475c2316bc19360bf9
Saba Ansari <saba.ansari0513@gmail.com>
2024-04-15 00:57:49 +0530

    All files

---
+++ routers/tables.py
@@ -0,0 +1,29 @@
+from fastapi import APIRouter
+from sqlalchemy import ARRAY, Date, Integer, PrimaryKeyConstraint, Column, String, DateTime, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.sql import func
+from . import engine
+
+router = APIRouter()
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = "creds"
+
+    email = Column(String)
+    mobile_number =  Column(String, default=None)
+    username = Column(String, primary_key=True, index=True)
+    password = Column(String)
+    name = Column(String)
+    date_of_birth = Column(Date, default=None)
+
+class Posts(Base):
+    __tablename__ = "posts"
+
+    post_id = Column(Integer, primary_key=True)
+    username = Column(String, ForeignKey("creds.username"))
+    image_file_name = Column(String)
+    caption = Column(String)
+
+Base.metadata.create_all(bind=engine)
