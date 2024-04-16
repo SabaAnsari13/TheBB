ef0dc68ed9010d5a3bc4e5e2e362eabfd2030386
SabaAnsari13 <122374567+SabaAnsari13@users.noreply.github.com>
2024-04-16 14:33:40 +0530

    Update __init__.py

--- routers/__init__.py
+++ routers/__init__.py
@@ -9,7 +9,7 @@ DB_HOST = os.getenv('DB_HOST')
 DB_PORT = os.getenv('DB_PORT')  # Default PostgreSQL port
 DB_NAME = os.getenv('DB_NAME')
 
-DATABASE_URL = f"postgresql://admin:sabzWork1305@db:5432/postgres"
+DATABASE_URL = f"postgresql://admin:sabzWork1305@localhost:5432/postgres"
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
