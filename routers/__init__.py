79b89f418dd2c8883df13803a80600d1788b5b1e
Saba Ansari <saba.ansari0513@gmail.com>
2024-04-16 15:13:08 +0530

    jenkins docker

--- routers/__init__.py
+++ routers/__init__.py
@@ -4,11 +4,6 @@ from sqlalchemy.orm import sessionmaker
 db_name="db"
 
 DATABASE_URL = f"postgresql://postgres:pOst31@host.docker.internal:5432/postgres"
-
-<<<<<<< HEAD
-=======
-DATABASE_URL = f"postgresql://admin:sabzWork1305@host.docker.internal:5432/postgres"
->>>>>>> c21823793f5cfd68c5f86413599045b7659b47ea
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
