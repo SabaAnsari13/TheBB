15b83315011f86d5f0f95d2eda6f9435f3f1f1be
Saba Ansari <saba.ansari0513@gmail.com>
2024-05-18 15:13:00 +0530

    Update

--- routers/__init__.py
+++ routers/__init__.py
@@ -3,7 +3,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 db_name="db"
 
-DATABASE_URL = f"postgresql://postgres:pOst31@localhost:5432/postgres"
+DATABASE_URL = f"postgresql://postgres:pOst31@host.docker.internal:5432/postgres"
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
